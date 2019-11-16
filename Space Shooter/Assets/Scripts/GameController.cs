903642ed9451937993c420cd8a2ca123f704d473
Paul Frappollo <55060905+PaulFrappollo21@users.noreply.github.com>
2019-11-16 18:38:10 -0500

    Challenge 3 Space Shooter

---
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public Text ScoreText;
+    private int score;
+    public Text restartText;
+    public Text gameOverText;
+    private bool gameOver;
+    private bool restart;
+    public Text winText;
+    void Start()
+    {
+        score = 0;
+        UpdateScore();
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        StartCoroutine (SpawnWaves());
+    }
+    void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown (KeyCode.Space))
+            {
+                SceneManager.LoadScene("Space Shooter");
+            }
+        }
+    }
+    IEnumerator SpawnWaves ()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+        yield return new WaitForSeconds(waveWait);
+        
+        if (gameOver)
+            {
+                restartText.text = "Press 'SpaceBar' to Restart";
+                restart = true;
+                break;
+            }
+        } 
+    }
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+    void UpdateScore()
+    {
+        ScoreText.text = "Points: " + score;
+        if (score >= 100)
+        {
+            winText.text = "You win! Game Created by Paul Frappollo";
+            restart = true;
+            gameOver = true;
+        }
+    }
+
+    public void GameOver ()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+  
+
+}
